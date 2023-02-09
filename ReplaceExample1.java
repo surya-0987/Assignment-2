b6ab62eef19104b7c6bfcafa55eff430c53395e8
surya-0987 <121616645+surya-0987@users.noreply.github.com>
2023-02-09 15:32:25 +0530

    Add files via upload

---
+++ ReplaceExample1.java
@@ -0,0 +1,8 @@
+public class ReplaceExample1 {
+    public static void main(String args[]) {
+        String s1 = "javatpoint is a very good website";
+        String replaceString = s1.replace('a', 'e');
+        System.out.println(replaceString);
+    }
+
+    }
---
+++ StartsWithExample.java
@@ -0,0 +1,10 @@
+public class StartsWithExample {
+    public static void main(String args[])
+    {
+// input string
+        String s1="java string split method by javatpoint";
+        System.out.println(s1.startsWith("ja"));
+        System.out.println(s1.startsWith("java string"));
+        System.out.println(s1.startsWith("Java string"));
+    }
+}
